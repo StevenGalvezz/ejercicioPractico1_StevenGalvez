ab11303c795db2940990faba862bf3e709eb1a3c
StevenGalvezz <cgalvez30381@ufide.ac.cr>
2025-10-22 18:32:36 -0600

    primer commit

---
+++ src/main/java/com/ejerpract1/Ejerpract1Application.java
@@ -0,0 +1,13 @@
+package com.ejerpract1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejerpract1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejerpract1Application.class, args);
+	}
+
+}
